0c9aa2e4f43286444b4c565c75a1a60f738b13bb
Dennis Tran <trandennis0@gmail.com>
2019-08-28 14:35:56 -0700

    add react routes

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import * as ROUTES from '../../constants/routes'
 
 const Navigation = () => (
   <div>
-    <h1>Navigation</h1>
+    <ul>
+      <li>
+        <NavLink to={ROUTES.SIGN_IN}>SIGN IN</NavLink>
+      </li>
+      <li>
+        <NavLink to={ROUTES.HOME}>HOME</NavLink>
+      </li>
+      <li>
+        <NavLink to={ROUTES.LANDING}>LANDING</NavLink>
+      </li>
+      <li>
+        <NavLink to={ROUTES.ACCOUNT}>ACCOUNT</NavLink>
+      </li>
+      <li>
+        <NavLink to={ROUTES.ADMIN}>ADMIN</NavLink>
+      </li>
+    </ul>
   </div>
 )
 
